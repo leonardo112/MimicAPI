e08df59cd339406fed722eaf11210f78f347d13d
leonardo ribeiro <leonardo.ribeirodasvirgens@gmail.com>
2020-09-30 00:26:54 -0300

    Migration Config

--- Startup.cs
+++ Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MimicAPI.Database;
 
 namespace MimicAPI
 {
@@ -16,6 +18,9 @@ namespace MimicAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MimicContext>(opt => 
+            opt.UseSqlite("Data Source=DataBase\\Mimic.db")
+            );
             services.AddMvc();
         }
 
